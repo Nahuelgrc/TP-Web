b4aa7158090443689fc9f2261073289b2a15b310
nahuelgrc <nahuelgrc@gmail.com>
2021-05-29 15:16:21 -0300

    Fix view

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,7 @@
     <q-header elevated class="bg-light-green">
       <q-toolbar>
         <q-btn
+          v-if="$store.state.user.isAdmin"
           flat
           dense
           round
@@ -10,21 +11,31 @@
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-        <q-toolbar-title href="/">
-          Shop
-        </q-toolbar-title>
-
-        <q-icon size="xl" name="account_circle" />
-        <q-btn flat rounded to="login">
-          Login
+        <router-link
+          to="/"
+          class="text-white text-bold"
+          style="text-decoration: none; font-size: 2.8em;"
+          >Shop</router-link
+        >
+        <q-space></q-space>
+        <q-btn flat round dense icon="shopping_cart">
+          <q-badge floating color="red">{{
+            $store.state.user.cart.length
+          }}</q-badge>
         </q-btn>
-        <q-btn rounded color="orange" to="signup">
-          Sign Up
-        </q-btn>
+        <q-btn flat round dense icon="account_circle" />
+        <div v-if="!$store.state.user.isLoggedIn">
+          <q-btn flat rounded to="login" float-right>
+            Login
+          </q-btn>
+          <q-btn rounded color="orange" to="signup">
+            Sign Up
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
-
     <q-drawer
+      v-if="leftDrawerOpen"
       v-model="leftDrawerOpen"
       show-if-above
       bordered
@@ -37,7 +48,6 @@
         <AdminPanel v-for="link in links" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
-
     <q-page-container class="bg-white">
       <router-view />
     </q-page-container>
